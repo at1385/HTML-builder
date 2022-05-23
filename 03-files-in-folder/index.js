a0a7ff152f6dde7cf3de57ae3af6305050631046
Alexander Tikhonov <at1385@yandex.ru>
2022-05-23 20:51:56 +0500

    feat: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const { promises, stat } = require('fs');
+const { readdir } = promises;
+
+async function showFilesInFolder() {
+  try {
+    const files = await readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true });
+
+    for (const file of files) {
+      if (file.isFile()) {
+        stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
+          if (err) return console.error(err.message);
+          console.log(`${path.parse(file.name).name} - ${path.extname(file.name)} - ${(stats.size / 1024).toFixed(3)}kb`);
+        });
+      }
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+showFilesInFolder();
